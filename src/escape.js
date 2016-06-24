c1a9246463ed16f815b99b56f7f89b93037b099f
Tristan Davies <github@tristan.io>
2016-06-24 13:37:36 -0400

    modify to work back to node 0.12

--- src/escape.js
+++ src/escape.js
@@ -2,7 +2,7 @@
 // ported from PostgreSQL 9.2.4 source code in src/interfaces/libpq/fe-exec.c
 
 module.exports = {
-  identifier (str) {
+  identifier: function identifier (str) {
     var escaped = '"'
     for (var i = 0; i < str.length; i++) {
       var c = str[i]
@@ -15,7 +15,7 @@ module.exports = {
     escaped += '"'
     return escaped
   },
-  literal (str) {
+  literal: function literal (str) {
     var hasBackslash = false
     var escaped = '\''
     for (var i = 0; i < str.length; i++) {
